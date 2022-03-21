282c3765b554b0241da170a6715aaf89e39ab6bc
gwoongg <00902bb@gmail.com>
2022-03-21 13:22:52 +0900

    source\20220316_롯데월드 프로그램.cpp 수정

--- source/20220316_롯데월드 프로그램.cpp
+++ source/20220316_롯데월드 프로그램.cpp
@@ -3,19 +3,58 @@
 
 int main()
 {
-	int ticketSelect, num, wooDae;
+	int ticketSelect, num, wooDae, todayYMD;
+    int year, month, day;
 	long long int juminnum;
 	char gender;
 	const int ADULT_DAY_PRICE = 56000, ADULT_NIGHT_PRICE = 45000,
 		    TEEN_DAY_PRICE = 50000, TEEN_NIGHT_PRICE = 40000,
 		    CHILD_DAY_PRICE = 46000, CHILD_NIGHT_PRICE = 35000,
-		    BABY_DAY_PRICE = 15000, BABY_NIGHT_PRICE = 15000;
+		    BABY_DAY_PRICE = 15000, BABY_NIGHT_PRICE = 15000,
+		    FREE_DAY_PRICE = 0, FREE_NIGHT_PRICE = 0;
 	const int MAX_NUM = 10, MIN_NUM = 1;	
 
+    todayYMD = year + month + day;
+    struct tm *today;
+	time_t timer;
+	time(&timer);
+	
+	today = localtime(&timer);
+	
+	timer = time(NULL);
+
+	year = (today -> tm_year - 100) * 10000;
+	month = (today -> tm_mon + 1) * 100;
+	day = today -> tm_mday;
+	
+	const long long int FULL_DIGIT = 10000000000000,
+				    FULL_DIGIT_MIN = 10000000000;
+				   
+	int age, generation;
+	
+	age = (todayYMD - (juminnum/10000000))/10000;
+	
+	if(((juminnum % 10000000) >= 1000000) && ((juminnum % 10000000) < 2000000 )){
+		generation = 1;	
+	}
+	else if(((juminnum % 10000000) >= 2000000) && ((juminnum % 10000000) < 3000000 )){
+		generation = 2;	
+	}
+	else if(((juminnum % 10000000) >= 3000000) && ((juminnum % 10000000) < 4000000 )){
+		generation = 3;
+	}
+	else if(((juminnum % 10000000) >= 4000000) && ((juminnum % 10000000) < 5000000 )){
+		generation = 4;
+	}
+	
+	if((generation == 1) || (generation == 2)){
+		age = age + 100;
+	} 
+	
 	while(true){
 	    printf("권종을 선택하세요.\n");
 	    printf("1. 1day\n");
-	    printf("2. After4\n");
+	    printf("2. After4(오후 4시부터 입장)\n");
 		scanf("%d", &ticketSelect);
 		if (ticketSelect == 1 || ticketSelect == 2){
 			break;		
@@ -25,15 +64,7 @@ int main()
 			printf("잘못된 입력입니다.\n\n");
 		}
     }
-	const long long int FULL_DIGIT = 10000000000000,
-				    FULL_DIGIT_MIN = 10000000000, 
-				    SEVEN_DIGIT = 1000000;
-	
-	const int TWO_DIGIT = 100, ONE_DIGIT = 10,
-		    OLD_GENERATION = 1900, NEW_GENERATION = 2000,
-		    MALE_OLD = 1, FEMALE_OLD = 2, MALE_NEW = 3, FEMALE_NEW = 4,
-		    BEFORE_BIRTH = 2, AFTER_BIRTH = 1;
-
+    
     while(true){
 	    printf("주민번호를 입력하세요.(- 제외 입력)\n");
 	    scanf("%lld", &juminnum);
@@ -72,8 +103,67 @@ int main()
 			printf("잘못된 입력입니다.\n\n");
 		    }
     }
-	int priceResult;
-	printf("\n\n가격은 %d 원 입니다.\n");
+    
+    int priceResult;
+    
+    if(ticketSelect == 1){ 
+		if(age < 1){
+			priceResult = FREE_DAY_PRICE;
+		} 
+		else if((age >= 1) && (age < 3)){
+			priceResult = BABY_DAY_PRICE; 
+		}
+		else if((age >= 3) && (age < 13)){
+			priceResult = CHILD_DAY_PRICE;
+		}
+		else if((age >= 13) && (age < 18)){
+			priceResult = TEEN_DAY_PRICE; 
+		}
+		else if(age >= 65){
+			priceResult = CHILD_DAY_PRICE; 
+		}
+		else{
+			priceResult = ADULT_DAY_PRICE;
+		} 
+		
+	}
+	else if(ticketSelect == 2){
+		if(age < 1){
+			priceResult = FREE_NIGHT_PRICE;
+		} 
+		else if((age >= 1) && (age < 3)){
+			priceResult = BABY_NIGHT_PRICE;  
+		}
+		else if((age >= 3) && (age < 13)){
+			priceResult = CHILD_NIGHT_PRICE;
+		}
+		else if((age >= 13) && (age < 18)){
+			priceResult = TEEN_NIGHT_PRICE; 
+		}
+		else if(age >= 65){
+			priceResult = CHILD_NIGHT_PRICE; 
+		}
+		else{
+			priceResult = ADULT_NIGHT_PRICE;
+		}
+	}
+	
+	if(wooDae == 2){
+		priceResult = priceResult * 0.5; 
+	}
+	if((wooDae == 3)){ 
+		priceResult = priceResult * 0.51;
+	} 
+	 
+	if((wooDae == 4)){
+		priceResult = priceResult * 0.5;
+	} 
+		
+	if((wooDae == 5)){
+		priceResult = priceResult * 0.7;
+	} 	
+	
+	printf("\n\n가격은 %d 원 입니다.\n", priceResult * num);
 	printf("감사합니다.\n\n");
 
 	return 0; 
