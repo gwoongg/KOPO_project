16233e717eb4374a4cf8efea7974f7663bd83ce9
gwoongg <00902bb@gmail.com>
2022-03-21 16:46:58 +0900

    업데이트

---
+++ 소프트웨어코딩/source1/배열 사용/배열 사용 006.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int decimal = 0;
+	int binary[4] = { 1, 1, 0, 1 };
+	
+	for(int index = 3; index >= 0; index --){
+		int result = 0;
+		for(int n = 0; n < index; n ++);
+		    result *= 2;
+	}	
+	printf("%d\n", decimal);
+	return 0;
+ } 
